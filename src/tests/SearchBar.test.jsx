b6e7028385860e06c65c27b4d01c9b7509d3678c
olivermooz-117 <olivermooz@gmail.com>
2026-08-18 14:06:21 +0300

    src/tests/searchbar.test.jsx

---
+++ src/tests/SearchBar.test.jsx
@@ -0,0 +1,59 @@
+export const mockResults = [
+  {
+    id: 1,
+    name: "Samsung Galaxy A51",
+    category: "Smartphones",
+    listings: [
+      {
+        eshop: "Jumia",
+        price: 30098,
+        deliveryCost: 200,
+        paymentMode: "Pay after delivery",
+        rating: 4.7,
+        ratingCount: 10,
+        bestValue: true,
+      },
+      {
+        eshop: "Kilimall",
+        price: 29999,
+        deliveryCost: 150,
+        paymentMode: "Pay before delivery",
+        rating: 4.0,
+        ratingCount: 4,
+        bestValue: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Bludio Wireless Headphones",
+    category: "Audio",
+    listings: [
+      {
+        eshop: "Jumia",
+        price: 5800,
+        deliveryCost: 200,
+        paymentMode: "Pay after delivery",
+        rating: 4.8,
+        ratingCount: 7,
+        bestValue: true,
+      },
+      {
+        eshop: "Kilimall",
+        price: 5589,
+        deliveryCost: 150,
+        paymentMode: "Pay before delivery",
+        rating: 5.0,
+        ratingCount: 1,
+        bestValue: false,
+      },
+    ],
+  },
+];
+
+export const trendingSearches = [
+  "Samsung A51",
+  "Wireless headphones",
+  "Laptop bag",
+  "Smart watch",
+];
